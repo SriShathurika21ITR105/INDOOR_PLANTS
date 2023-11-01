94bc3ad8bc957599d14eca535162f9d7af804f76
Sri Shathurika <118598976+SriShathurika21ITR105@users.noreply.github.com>
2023-11-01 14:27:48 +0530

    Update LoginPage.jsx

--- src/components/Header/LoginPage.jsx
+++ src/components/Header/LoginPage.jsx
@@ -85,7 +85,6 @@ import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './LoginPage.scss';
 import backgroundImage from "../../assets/images/backgroundImage.jpg";
-//import loginImage from './login.jpg'; // Import your login image
 
 function LoginPage() {
   const [username, setUsername] = useState('');
@@ -100,80 +99,79 @@ function LoginPage() {
       return;
     }
 
-    try {
-      const response = await fetch('/coachlog', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+    // Check the password
+    if (password === 'sri') {
+      // Password is correct, proceed with login
+      try {
+        const response = await fetch('/coachlog', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
 
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
+        if (response.ok) {
+          const data = await response.json();
+          localStorage.setItem('token', data.token);
    
-        alert('Login Successfull');
-        navigate('/');
-      } else {
-        alert('Invalid username or password'); 
+          alert('Login Successful');
+          navigate('/');
+        } else {
+          alert('Invalid username or password'); 
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred during login'); 
       }
-    } catch (error) {
-      console.error('Error:', error);
-      alert('An error occurred during login.'); 
+    } else {
+      // Password is incorrect
+      alert('Invalid password'); 
     }
   };
-  const backgroundImageUrl='https://img.freepik.com/premium-photo/modern-loft-empty-living-room-interior-design-black-pattern-wall-background_221619-1792.jpg?w=1060';
-  return (
 
-    <div className='log'
-    style={{
-      backgroundImage: `url(${backgroundImageUrl})`, // Set the online image URL
+  const backgroundImageUrl = 'https://img.freepik.com/premium-photo/modern-loft-empty-living-room-interior-design-black-pattern-wall-background_221619-1792.jpg?w=1060';
+
+  return (
+    <div className='log' style={{
+      backgroundImage: `url(${backgroundImageUrl})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }}>
-      
-    <div className="login-container">
-      <div className="login-content">
-        <div className="login-form">
-          <h2>Sign In</h2>
-      <br></br>
-          <form onSubmit={handleLogin}>
-            <div className="form-group">
-              
-            <input
+      <div className="login-container">
+        <div className="login-content">
+          <div className="login-form">
+            <h2>Sign In</h2>
+            <br></br>
+            <form onSubmit={handleLogin}>
+              <div className="form-group">
+                <input
                   type="text"
                   id="input1"
                   name="input1"
-                
                   required
                   placeholder="Username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                 />
-            </div>
-            <div className="form-group">
-            <input
+              </div>
+              <div className="form-group">
+                <input
                   type="password"
                   id="input2"
                   name="input2"
-               
                   required
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-            </div>
-            <button type="submit">Login</button><br/><br/>
-            Don't have a account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
-          </form>
+              </div>
+              <button type="submit">Login</button><br/><br/>
+              Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+            </form>
+          </div>
         </div>
-        {/* <div className="login-image">
-          <img src={loginImage} alt="Login" />
-         </div> */}
-        
       </div>
-    </div>
     </div>
   );
 }
