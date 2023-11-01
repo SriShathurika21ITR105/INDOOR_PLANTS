e396316957f010c1df1218cc9e0c8ddd7ba650d8
Sri Shathurika <118598976+SriShathurika21ITR105@users.noreply.github.com>
2023-11-01 14:36:29 +0530

    Update LoginPage.jsx

--- src/components/Header/LoginPage.jsx
+++ src/components/Header/LoginPage.jsx
@@ -80,61 +80,138 @@
 // export default LoginPage;
 
 // LoginPage.js
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import './LoginPage.scss';
+// import backgroundImage from "../../assets/images/backgroundImage.jpg";
+
+// function LoginPage() {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+  
+//   const navigate = useNavigate();
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     if (!username || !password) {
+//       alert('Please fill in all fields'); 
+//       return;
+//     }
+
+//     // Check the password
+//     if (password === 'sri') {
+//       // Password is correct, proceed with login
+//       try {
+//         const response = await fetch('/coachlog', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({ username, password }),
+//         });
+
+//         if (response.ok) {
+//           const data = await response.json();
+//           localStorage.setItem('token', data.token);
+   
+//           alert('Login Successful');
+//           navigate('/');
+//         } else {
+//           alert('Invalid username or password'); 
+//         }
+//       } catch (error) {
+//         console.error('Error:', error);
+//         alert('An error occurred during login'); 
+//       }
+//     } else {
+//       // Password is incorrect
+//       alert('Invalid password'); 
+//     }
+//   };
+
+//   const backgroundImageUrl = 'https://img.freepik.com/premium-photo/modern-loft-empty-living-room-interior-design-black-pattern-wall-background_221619-1792.jpg?w=1060';
+
+//   return (
+//     <div className='log' style={{
+//       backgroundImage: `url(${backgroundImageUrl})`,
+//       backgroundSize: 'cover',
+//       backgroundPosition: 'center',
+//     }}>
+//       <div className="login-container">
+//         <div className="login-content">
+//           <div className="login-form">
+//             <h2>Sign In</h2>
+//             <br></br>
+//             <form onSubmit={handleLogin}>
+//               <div className="form-group">
+//                 <input
+//                   type="text"
+//                   id="input1"
+//                   name="input1"
+//                   required
+//                   placeholder="Username"
+//                   value={username}
+//                   onChange={(e) => setUsername(e.target.value)}
+//                 />
+//               </div>
+//               <div className="form-group">
+//                 <input
+//                   type="password"
+//                   id="input2"
+//                   name="input2"
+//                   required
+//                   placeholder="Password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                 />
+//               </div>
+//               <button type="submit">Login</button><br/><br/>
+//               Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LoginPage;
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './LoginPage.scss';
-import backgroundImage from "../../assets/images/backgroundImage.jpg";
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  
   const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
     if (!username || !password) {
-      alert('Please fill in all fields'); 
+      alert('Please fill in all fields');
       return;
     }
 
-    // Check the password
-    if (password === 'sri') {
-      // Password is correct, proceed with login
-      try {
-        const response = await fetch('/coachlog', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username, password }),
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          localStorage.setItem('token', data.token);
-   
-          alert('Login Successful');
-          navigate('/');
-        } else {
-          alert('Invalid username or password'); 
-        }
-      } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred during login'); 
-      }
+    // Replace this hardcoded password check with your secure authentication method
+    if (password === 'subhi') {
+      // For demo purposes, the password 'subhi' grants access
+      localStorage.setItem('token', 'yourGeneratedTokenHere');
+      alert('Login Successful');
+      navigate('/');
     } else {
-      // Password is incorrect
-      alert('Invalid password'); 
+      alert('Invalid username or password');
     }
   };
 
-  const backgroundImageUrl = 'https://img.freepik.com/premium-photo/modern-loft-empty-living-room-interior-design-black-pattern-wall-background_221619-1792.jpg?w=1060';
+  const backgroundImageUrl = 'https://img.freepik.com/free-photo/empty-modern-room-with-furniture_23-2149178345.jpg?w=1060&t=st=1697099305~exp=1697099905~hmac=b7217fcf8e37934360faa7ee0becb0e69a816253118e53683ddc39bb302e385f'; // Replace with the actual image URL
 
   return (
     <div className='log' style={{
-      backgroundImage: `url(${backgroundImageUrl})`,
+      backgroundImage: `url(${backgroundImageUrl})`, // Set the online image URL
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }}>
@@ -142,7 +219,7 @@ function LoginPage() {
         <div className="login-content">
           <div className="login-form">
             <h2>Sign In</h2>
-            <br></br>
+            <p>“Relax we've got this!”</p>
             <form onSubmit={handleLogin}>
               <div className="form-group">
                 <input
@@ -166,8 +243,12 @@ function LoginPage() {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <button type="submit">Login</button><br/><br/>
-              Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+              <button type="submit">Login</button>
+              <br/><br/>
+              Don't have an account?
+              <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>
+                Create a new account
+              </Link>
             </form>
           </div>
         </div>
@@ -177,3 +258,4 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
